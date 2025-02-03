00f05f544c35d4f4a46438efaf2fa95760ceed14
Halo333X <124174275+Halo333X@users.noreply.github.com>
2025-02-02 18:19:54 -0600

    Website now plays addon music

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+const tracks = [
+  "./music/1.ogg",
+  "./music/2.ogg",
+  "./music/3.ogg",
+  "./music/4.ogg",
+  "./music/5.ogg",
+  "./music/6.ogg"
+];
+
 window.onload = () => {
   setTimeout(() => {
     document.querySelector(".overlay").style.opacity = "1";
@@ -5,4 +14,14 @@ window.onload = () => {
     content.style.opacity = "1";
     content.style.transform = "translateY(0)";
   }, 1000);
+
+  const music = document.getElementById('background-music');
+  
+  window.addEventListener('click', () => {
+    if (music.paused) {
+      music.src = tracks[Math.floor(Math.random() * tracks.length)];
+      music.volume = 0.2;
+      music.play().catch(error => console.error('Playback error:', error));
+    }
+  });
 };
